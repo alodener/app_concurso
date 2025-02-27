c62e0c98cd43b0c99bbe08e276b632b1be5806eb
Dener Gomes <52760360+alodener@users.noreply.github.com>
2025-02-26 23:12:21 -0300

    Update EnviarResultado.vue

--- src/views/Modulos/EnviarResultado.vue
+++ src/views/Modulos/EnviarResultado.vue
@@ -60,11 +60,15 @@
                 <option value="chanchito_Loto">Chanchito Loto</option>
                 <option value="easy_power_loto">Easy Power Loto</option>
                 <option value="chao_jefe_loto">Chao Jefe Loto</option>
-                <option value="chispaloto_segundo">Chispaloto Segundo</option>
+                <option value="chispaloto_noite">Chispaloto Noite</option>
                 <option value="mega_lotto">Mega Lotto</option>
                 <option value="mega_kino">Mega Kino</option>
                 <option value="santa_lucia_double">Santa Lucia Double</option>
                 <option value="super_quina">Super Quina</option>
+                <option value="cash_life">Cash Life</option>
+                <option value="ny_lotto">Cash Life</option>
+                <option value="mega_power">Mega Power</option>
+                <option value="la_tinka">La Tinka</option>
               </CFormSelect>
             </CInputGroup>
             <CInputGroup class="mt-4">
